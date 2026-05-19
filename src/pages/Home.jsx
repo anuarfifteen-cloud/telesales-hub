168e4986527ab9775574bc5975ff2dc423f90966
Mohammad Anwaruddin bin Embran <anwaruddin.embran@dst.com.bn>
2026-05-19 07:58:17 +0000

    Manual code change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -617,7 +617,7 @@ export default function Home() {
                           </p>
                         ) : (
                           <p className="text-[10px] text-slate-400 dark:text-slate-500 leading-none mt-0.5">
-                            🔒 Locked ({totalCount}/15 bookings)
+                            Hit 15 Boooking to Unlock! ({totalCount}/15 bookings)
                           </p>
                         )}
                       </div>
